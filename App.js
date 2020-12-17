c1a29d8f5661ce03d76ff970486c6bd0ae68f9fa
Imad EL Kholti <im.el-kholti@domain.com>
2020-12-17 11:23:30 +0100

    d

--- App.js
+++ App.js
@@ -1,27 +1,35 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen  from "./Pages/Home";
 import MovieScreen from "./Pages/Movie";
 import WebViewScreen from "./Pages/webView";
 import SettingsScreen from "./Pages/Settings";
 import Watch_WVScreen from "./Pages/Watch_WV";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 
 
 console.disableYellowBox = true;
-const AppNavigator = createStackNavigator(
-  {
-    Home      : HomeScreen,
-    Movie     : MovieScreen,
-    WebViewer : WebViewScreen,
-    Settings  : SettingsScreen,
-    Watch_WV  : Watch_WVScreen,
-  },
-  {
-    initialRouteName: 'Home',
-  }
-);
+const AppNavigator = createStackNavigator();
 
-export default createAppContainer(AppNavigator);
+function MyStack() {
+  return (
+    <AppNavigator.Navigator>
+      <AppNavigator.Screen name="Home" component={HomeScreen} />
+      <AppNavigator.Screen name="Movie" component={MovieScreen} />
+      <AppNavigator.Screen name="WebViewer" component={WebViewScreen} />
+      <AppNavigator.Screen name="Settings" component={SettingsScreen} />
+      <AppNavigator.Screen name="Watch_WV" component={Watch_WVScreen} />
+    </AppNavigator.Navigator>
+  );
+}
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
